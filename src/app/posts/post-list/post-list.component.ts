91eb6a601b22784a32633c501c26a812a6604ea4
Arunbaby <arunbaby.janardhanan@griffithuni.edu.au>
2019-05-14 13:49:01 +1000

    angular forms completed

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
@@ -9,12 +11,28 @@ import { Post } from '../post.model';
 })
 
 
-export class PostListComponent {
+export class PostListComponent implements OnInit, OnDestroy {
   // posts = [
   //   {title: 'First Post', content: "This is the First post's content"},
   //   {title: 'Second Post', content: "This is the Second post's content"},
   //   {title: 'Third Post', content: "This is the Third post's content"}
   // ];
 
-  @Input() posts: Post[] = [];
+  posts: Post[] = [];
+  private postsSub: Subscription;
+
+
+  constructor(public postsService: PostsService) {}
+
+  ngOnInit() {
+    this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener()
+    .subscribe((posts: Post[]) => {
+      this.posts = posts;
+    });
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
 }
---
+++ src/app/posts/posts.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+import { Post } from './post.model';
+
+@Injectable({providedIn: 'root'})
+
+export class PostsService {
+  private posts: Post[] = [];
+  private postsUpdated = new Subject<Post[]>();
+
+  getPosts() {
+    return [...this.posts];
+  }
+
+  getPostUpdateListener() {
+    return this.postsUpdated.asObservable();
+  }
+
+  addPost(title: string, content: string) {
+    const post: Post = {title, content};
+    this.posts.push(post);
+    this.postsUpdated.next([...this.posts]);
+  }
+}
+
+
+
+
+
+// @Injectable({providedIn: 'root'})
+
+// export class PostsService {
+//   private posts: Post[] = [];
+
+//   getPosts() {
+//     return [...this.posts];
+//   }
+
+//   addPost(title: string, content: string) {
+//     const post: Post = {title: title, content: content};
+//     this.posts.push(post);
+//   }
+// }
